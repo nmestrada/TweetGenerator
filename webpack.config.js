6403fd4ea291c32525b150fbe438910a3b3d63df
Natalie Estrada <nmestrad@gmail.com>
2020-03-30 16:36:27 -0500

    added travis and heroku

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,29 @@
+const isDev = process.env.NODE_ENV === 'development'
+
 module.exports = {
-    entry: './index.js',// assumes your entry point is the index.js in the root of your project folder
-    mode: 'development',
-    output: {
-      path: __dirname, // assumes your bundle.js will also be in the root of your project folder
-      filename: './public/bundle.js'
-    },
-    devtool: 'source-maps',
-    module: {
-        rules: [
-          {
-            test: /\.jsx?$/,
-            exclude: /(node_modules|bower_components)/,
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/react', '@babel/env']
-            }
-          },
-          // use the style-loader/css-loader combos for anything matching the .css extension
-          {
-            test: /\.css$/,
-            use: [
-              'style-loader',
-              'css-loader',
-            ]
-          }
-        ]
+  mode: isDev ? 'development' : 'production',
+  entry: [
+    '@babel/polyfill', // enables async-await
+    './client/index.js'
+  ],
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  devtool: 'source-map',
+  watchOptions: {
+    ignored: /node_modules/
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
       }
+    ]
   }
+}
